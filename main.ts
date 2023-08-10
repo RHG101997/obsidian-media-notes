0ccfb553930089f856b7cd73df0e78a2fdeeec9a
rhg101997 <3848345+RHG101997@users.noreply.github.com>
2023-08-10 01:19:42 -0400

    First commit, adding media and cosmetic changes

--- main.ts
+++ main.ts
@@ -1,134 +1,272 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import ReactPlayer from 'react-player/lazy'
+
+import { VideoView, VIDEO_VIEW } from './view_container/videoview';
+// import { TimestampPluginSettings } from 'setting';
+
+
+const ERRORS: { [key: string]: string } = {
+	"INVALID_URL": "\n> [!error] Invalid Video URL\n> The highlighted link is not a valid video url. Please try again with a valid link.\n",
+	"NO_ACTIVE_VIDEO": "\n> [!caution] Select Video\n> A video needs to be opened before using this hotkey.\n Highlight your video link and input your 'Open video player' hotkey to register a video.\n",
+}
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	noteTitle: string;
+	urlStartTimeMap: Map<string, number>;
+	urlColor: string;
+	timestampColor: string;
+	urlTextColor: string;
+	timestampTextColor: string;
+	forwardSeek: string;
+	backwardsSeek: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	noteTitle: "",
+	urlStartTimeMap: new Map<string, number>(),
+	urlColor: 'blue',
+	timestampColor: 'green',
+	urlTextColor: 'white',
+	timestampTextColor: 'white',
+	forwardSeek: '10',
+	backwardsSeek: '10'
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	player: ReactPlayer;
+	setPlaying: React.Dispatch<React.SetStateAction<boolean>>;
+	editor: Editor;
 
 	async onload() {
+
+		// Register view
+		this.registerView(
+			VIDEO_VIEW,
+			(leaf) => new VideoView(leaf)
+		);
+
+			// Register settings
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('video', 'Play Media', (evt: MouseEvent ) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+			
+			if (activeView) {
+				new Notice('Opening video!');
+
+				if (!this.editor) {
+					this.editor = activeView.editor;
+					}
+				const url = this.editor.getSelection().trim();
+				this.activateView(url, this.editor);
+			}else{
+				new Notice(ERRORS["NO_ACTIVE_VIDEO"]);
+			}
+			
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('Media-Notes Running!');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+
+		this.registerMarkdownCodeBlockProcessor("timestamp", (source, el, ctx) => {
+			const regExpWithHint = /(\d+:\d+:\d+|\d+:\d+)\s*:\s*(.+)/;
+			const regExpWithoutHint = /\d+:\d+:\d+|\d+:\d+/g;
+			const rows = source.split("\n").filter((row) => row.length > 0);
+			rows.forEach((row) => {
+			const matchWithHint = regExpWithHint.exec(row);
+			const matchWithoutHint = row.match(regExpWithoutHint);
+			
+			if (matchWithHint || matchWithoutHint) {
+				// create a button for each timestamp
+				const div = el.createEl("div");
+				const button = div.createEl("button");
+
+				let timestamp: any;
+				if (matchWithHint) {
+					timestamp = matchWithHint[1];
+					button.innerText = timestamp;
+				
+					// create a text element for the hint
+					const hintElement = div.createEl("span");
+					hintElement.className = 'timestamp-hint'; // Add this class for styling
+					hintElement.innerText = matchWithHint[2]; // Matched hint
+					div.appendChild(button); // Append the button first
+					div.appendChild(hintElement); // Append the hint text next to the button
+				} else {
+					timestamp = matchWithoutHint[0];
+					button.innerText = timestamp;
+					div.appendChild(button);
+				}
+				
+				button.className = 'timestamp-button'; // Add this class for styling
+				
+				// convert timestamp to seconds and seek to that position when clicked
+				button.addEventListener("click", () => {
+					const timeArr = timestamp.split(":").map((v:any) => parseInt(v)); 
+					const [hh, mm, ss] = timeArr.length === 2 ? [0, ...timeArr] : timeArr;
+					const seconds = (hh || 0) * 3600 + (mm || 0) * 60 + (ss || 0);
+					if (this.player) this.player.seekTo(seconds);
+				});
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+
+		});
+
+
+		this.registerMarkdownCodeBlockProcessor("timestamp-url", (source, el, ctx) => {
+			const url = source.trim();
+			if (ReactPlayer.canPlay(url)) {
+				// Creates button for video url
+				const div = el.createEl("div");
+				const button = div.createEl("button");
+				button.innerText = url;
+				button.className = 'url-button';
+
+				button.addEventListener("click", () => {
+					this.activateView(url, this.editor);
+				});
+			} else {
+				if (this.editor) {
+					this.editor.replaceSelection(this.editor.getSelection() + "\n" + ERRORS["INVALID_URL"]);
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: 'open-media-player',
+			name: 'Media Player (simple)',
+			editorCallback: (editor: Editor, view: MarkdownView) =>{
+				const url = editor.getSelection().trim();
+				this.activateView(url, editor);
+			}
+		});
+		this.addCommand({
+			id: 'test-media-player',
+			name: 'TEST Media Player VIDEO',
+			editorCallback: (editor: Editor, view: MarkdownView) =>{
+				this.activateView(`https://www.youtube.com/watch?v=487AjvFW1lk`, editor);
+			}
+		});
+	}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+	async activateView(url: string, editor: Editor) {
+		this.app.workspace.detachLeavesOfType(VIDEO_VIEW);
+
+		await this.app.workspace.getRightLeaf(false).setViewState({
+			type: VIDEO_VIEW,
+			active: true,
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.app.workspace.revealLeaf(
+			this.app.workspace.getLeavesOfType(VIDEO_VIEW)[0]
+		);
+				// This triggers the React component to be loaded
+				this.app.workspace.getLeavesOfType(VIDEO_VIEW).forEach(async (leaf) => {
+					if (leaf.view instanceof VideoView) {
+		
+						const setupPlayer = (player: ReactPlayer, setPlaying: React.Dispatch<React.SetStateAction<boolean>>) => {
+							this.player = player;
+							this.setPlaying = setPlaying;
+						}
+		
+						const setupError = (err: string) => {
+							editor.replaceSelection(editor.getSelection() + `\n> [!error] Streaming Error \n> ${err}\n`);
+						}
+		
+						const saveTimeOnUnload = async () => {
+							if (this.player) {
+								this.settings.urlStartTimeMap.set(url, Number(this.player.getCurrentTime().toFixed(0)));
+							}
+							await this.saveSettings();
+						}
+		
+						// create a new video instance, sets up state/unload functionality, and passes in a start time if available else 0
+						leaf.setEphemeralState({
+							url,
+							setupPlayer,
+							setupError,
+							saveTimeOnUnload,
+							start: ~~this.settings.urlStartTimeMap.get(url)
+						});
+
+
+						await this.saveSettings();
+					}		
+				});
+	}			
+
+	async loadSettings() {
+		// Fix for a weird bug that turns default map into a normal object when loaded
+		const data = await this.loadData()
+		if (data) {
+			const map = new Map(Object.keys(data.urlStartTimeMap).map(k => [k, data.urlStartTimeMap[k]]))
+			this.settings = { ...DEFAULT_SETTINGS, ...data, urlStartTimeMap: map };
+		} else {
+			this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		}
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.player = null;
+		this.editor = null;
+		this.setPlaying = null;
+		this.app.workspace.detachLeavesOfType(VIDEO_VIEW);
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+		// await this.saveData(this.settings);
 	}
 }
+	
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// class SampleModal extends Modal {
+// 	constructor(app: App) {
+// 		super(app);
+// 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+// 	onOpen() {
+// 		const {contentEl} = this;
+// 		contentEl.setText('Woah!');
+// 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+// 	onClose() {
+// 		const {contentEl} = this;
+// 		contentEl.empty();
+// 	}
+// }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+// class SampleSettingTab extends PluginSettingTab {
+// 	plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+// 	constructor(app: App, plugin: MyPlugin) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
 
-	display(): void {
-		const {containerEl} = this;
+// 	display(): void {
+// 		const {containerEl} = this;
 
-		containerEl.empty();
+// 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+// 		new Setting(containerEl)
+// 			.setName('Setting #1')
+// 			.setDesc('It\'s a secret')
+// 			.addText(text => text
+// 				.setPlaceholder('Enter your secret')
+// 				.setValue(this.plugin.settings.mySetting)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.mySetting = value;
+// 					await this.plugin.saveSettings();
+// 				}));
+// 	}
+// }
---
+++ setting.ts
@@ -0,0 +1,135 @@
+import { App, PluginSettingTab, Setting } from 'obsidian';
+import TimestampPlugin from './main';
+
+export interface TimestampPluginSettings {
+	noteTitle: string;
+	urlStartTimeMap: Map<string, number>;
+	urlColor: string;
+	timestampColor: string;
+	urlTextColor: string;
+	timestampTextColor: string;
+	forwardSeek: string;
+	backwardsSeek: string;
+}
+
+export const DEFAULT_SETTINGS: TimestampPluginSettings = {
+	noteTitle: "",
+	urlStartTimeMap: new Map<string, number>(),
+	urlColor: 'blue',
+	timestampColor: 'green',
+	urlTextColor: 'white',
+	timestampTextColor: 'white',
+	forwardSeek: '10',
+	backwardsSeek: '10'
+}
+
+const COLORS = { 'blue': 'blue', 'red': 'red', 'green': 'green', 'yellow': 'yellow', 'orange': 'orange', 'purple': 'purple', 'pink': 'pink', 'grey': 'grey', 'black': 'black', 'white': 'white' };
+
+const TIMES = { '5': '5', '10': '10', '15': '15', '20': '20', '25': '25', '30': '30', '35': '35', '40': '40', '45': '45', '50': '50', '55': '55', '60': '60', '65': '65', '70': '70', '75': '75', '80': '80', '85': '85', '90': '90', '95': '95', '100': '100', '105': '105', '110': '110', '115': '115', '120': '120' }
+
+// export class TimestampPluginSettingTab extends PluginSettingTab {
+// 	plugin: TimestampPlugin;
+
+// 	constructor(app: App, plugin: TimestampPlugin) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
+
+// 	display(): void {
+// 		const { containerEl } = this;
+
+// 		containerEl.empty();
+
+// 		containerEl.createEl('h2', { text: 'Timestamp Notes Plugin' });
+
+// 		// Customize title
+// 		new Setting(containerEl)
+// 			.setName('Title')
+// 			.setDesc('This title will be printed after opening a video with the hotkey. Use <br> for new lines.')
+// 			.addText(text => text
+// 				.setPlaceholder('Enter title template.')
+// 				.setValue(this.plugin.settings.noteTitle)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.noteTitle = value;
+// 					await this.plugin.saveSettings();
+// 				}));
+
+// 		// Customize  url button color
+// 		new Setting(containerEl)
+// 			.setName('URL Button Color')
+// 			.setDesc('Pick a color for the url button.')
+// 			.addDropdown(dropdown => dropdown
+// 				.addOptions(COLORS)
+// 				.setValue(this.plugin.settings.urlColor)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.urlColor = value;
+// 					await this.plugin.saveSettings();
+// 				}
+// 				));
+
+// 		// Customize url text color
+// 		new Setting(containerEl)
+// 			.setName('URL Text Color')
+// 			.setDesc('Pick a color for the URL text button.')
+// 			.addDropdown(dropdown => dropdown
+// 				.addOptions(COLORS)
+// 				.setValue(this.plugin.settings.urlTextColor)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.urlTextColor = value;
+// 					await this.plugin.saveSettings();
+// 				}
+// 				));
+
+// 		// Customize timestamp button color
+// 		new Setting(containerEl)
+// 			.setName('Timestamp Button Color')
+// 			.setDesc('Pick a color for the timestamp button.')
+// 			.addDropdown(dropdown => dropdown
+// 				.addOptions(COLORS)
+// 				.setValue(this.plugin.settings.timestampColor)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.timestampColor = value;
+// 					await this.plugin.saveSettings();
+// 				}
+// 				));
+
+// 		// Customize timestamp text color
+// 		new Setting(containerEl)
+// 			.setName('Timestamp Text Color')
+// 			.setDesc('Pick a color for the timestamp text.')
+// 			.addDropdown(dropdown => dropdown
+// 				.addOptions(COLORS)
+// 				.setValue(this.plugin.settings.timestampTextColor)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.timestampTextColor = value;
+// 					await this.plugin.saveSettings();
+// 				}
+// 				));
+
+// 		// Customize forward seek time
+// 		new Setting(containerEl)
+// 			.setName('Foward time seek')
+// 			.setDesc('This is the amount of seconds the video will seek forward when pressing the seek forward command.')
+// 			.addDropdown(dropdown => dropdown
+// 				.addOptions(TIMES)
+// 				.setValue(this.plugin.settings.forwardSeek)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.forwardSeek = value;
+// 					await this.plugin.saveSettings();
+// 				}
+// 				));
+
+// 		// Customize backwards seek time
+// 		new Setting(containerEl)
+// 			.setName('Backwards time seek')
+// 			.setDesc('This is the amount of seconds the video will seek backwards when pressing the seek backwards command.')
+// 			.addDropdown(dropdown => dropdown
+// 				.addOptions(TIMES)
+// 				.setValue(this.plugin.settings.backwardsSeek)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.backwardsSeek = value;
+// 					await this.plugin.saveSettings();
+// 				}
+// 				));
+// 	}
+// }
